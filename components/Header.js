7d45f2f9845047ccd47e468b05b3cceffdc28b71
alkun <zhujunkun@yit.com>
2019-10-11 15:27:15 +0800

    feat: 首页的架构搭建

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../static/style/header.css'
+
+const Header = () => {
+    return <div className="Header">
+        <h2>HeadHeadhead</h2>
+    </div>
+}
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+// import Link from 'next/link'
+// import Head from 'next/head'
 import Router from 'next/router'
-import {Button} from 'antd'
-import Nav from '../components/nav'
+import { Layout } from 'antd'
 
+import Header from '../components/Header.js'
+import SiderBox from '../components/Sider.js'
+import ListBox from '../components/ListBox.js'
+import '../static/style/index.css'
+
+
+const { Sider, Content } = Layout
 const Home = () => {
-  // 路由狗子函数:
+  // 路由钩子函数:
   // routeChangeStart
   // routeChangeComplete
   // beforeHistoryChange
@@ -21,61 +27,18 @@ const Home = () => {
   })
 
 
-  return <div>
-
-    <Link href="/test?aa=11&bb=222"><a>alkun</a></Link>
-    <p onClick={() => {
-      Router.push({pathname:'/alkun', query: {
-        aa: 666,
-        bb: 999
-      }})
-    }}>11111</p>
-    <Button>点击</Button>
+  return <div className="home">
+    <Header />
+    <Layout>
+        <Sider width="15vw">
+          <SiderBox></SiderBox>
+        </Sider>
+        <Content>
+          <ListBox></ListBox>
+        </Content>
+    </Layout>
     <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
+     
     `}</style>
   </div>
 }
